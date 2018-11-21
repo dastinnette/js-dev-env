27a13ccd5a6fc0527426dc980e52a862c5bc1c5a
David Stinnette <dastinnette@gmail.com>
2018-11-21 14:51:26 -0500

    Configured eslint

--- src/index.js
+++ src/index.js
@@ -2,5 +2,4 @@ import './index.css'
 import numeral from 'numeral';
 
 const storedValue = numeral(1000).format('$0,0.00');
-debugger;
-console.log(`I would pay ${storedValue} for that car`);
+console.log(`I would pay ${storedValue} for that car`); // eslint-disable-line no-console
