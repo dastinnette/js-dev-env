719ef0de82b7d9e8678d2cc8ebdb75d376fc74fd
David Stinnette <dastinnette@gmail.com>
2018-11-23 13:45:27 -0500

    Configured fetch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-import './index.css'
-import numeral from 'numeral';
+import {getUsers} from './api/userApi';
 
-const storedValue = numeral(1000).format('$0,0.00');
-console.log(`I would pay ${storedValue} for that car`); // eslint-disable-line no-console
+getUsers().then(result => {
+  let usersBody = "";
+
+  result.forEach(user => {
+    usersBody+= `<tr>
+      <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+      <td>${user.id}</td>
+      <td>${user.name}</td>
+      <td>${user.email}</td>
+      </tr>`
+  });
+
+  global.document.getElementById('users').innerHTML = usersBody;
+});
